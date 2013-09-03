8db337d7e239c8f8a12416d48fc78d5f4ed1a2a4
John Smith <da.capo.al.fine@hotmail.com>
2013-09-03 19:11:54 +0400

    Add 'not-random' API - a rough and very simple randomness test.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Public Domain (Unlicense)"
             :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.bouncycastle/bcpg-jdk15on "1.49"]]
+                 [org.bouncycastle/bcpg-jdk15on "1.49"]
+                 [org.apache.commons/commons-math3 "3.2"]]
   :main crypto.openpgp)
--- test/crypto/openpgp_test.clj
+++ test/crypto/openpgp_test.clj
@@ -1,6 +1,29 @@
 (ns crypto.openpgp-test
-  (:use clojure.test crypto.openpgp))
+  (:use clojure.test crypto.openpgp)
+  (:import [java.util Random]))
+
+(defn- working-random []
+  (new java.util.Random 1))
+
+; A less uniform distribution.
+(defn- broken-random []
+  (let [random (working-random)]
+    (proxy [java.util.Random] [0]
+      (setSeed [seed]
+        (.setSeed random seed))
+      (next [n]
+        (let [r1 (.nextInt random),
+              r2 (.nextInt random),
+              r3 (.nextInt random),
+              r4 (.nextInt random)]
+          (bit-and (bit-shift-right (+ r1 r2 r3 r4) 2) (- (bit-shift-left 1 n) 1)))))))
+
+(defn- run-nr-test [random]
+  (not (not-random random 64 100000 0.001)))
 
 (deftest a-test
   (testing "library-version"
-    (is (re-matches #"(?>0|[1-9][0-9]*)\.(?>0|[1-9][0-9]*)\.(?>0|[1-9][0-9]*)" (library-version)))))
+    (is (re-matches #"(?>0|[1-9][0-9]*)\.(?>0|[1-9][0-9]*)\.(?>0|[1-9][0-9]*)" (library-version))))
+  (testing "not-random"
+    (is (run-nr-test (working-random)))
+    (is (not (run-nr-test (broken-random))))))
