a19879ed13261e3e1d66f2f1fe50da6b5606545a
John Smith <da.capo.al.fine@hotmail.com>
2013-10-16 14:52:15 +0400

    Reformat comments as Markdown docstrings.

--- project.clj
+++ project.clj
@@ -6,4 +6,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.bouncycastle/bcpg-jdk15on "1.49"]
                  [org.apache.commons/commons-math3 "3.2"]]
+  :profiles {:codox {:plugins [[codox "0.6.6"]]
+                     :dependencies [[codox-md "0.2.0"]]
+                     :codox {:writer codox-md.writer/write-docs
+                             :src-dir-uri "https://github.com/MailTube/crypto.openpgp/blob/master/"
+                             :src-linenum-anchor-prefix "L"}}}
   :main crypto.openpgp)
